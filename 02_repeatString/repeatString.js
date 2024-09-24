13ee01b163110e50254a1e53690bbd24719991ad
sci-punk <ishen.sarabia@gmail.com>
2024-09-24 00:13:51 -0600

    02_repeatString solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let stringToRepeat = ""
+    for (let index = 0; index < number; index++) {
+        stringToRepeat += string        
+    }
+    return stringToRepeat
 };
 
 // Do not edit below this line
