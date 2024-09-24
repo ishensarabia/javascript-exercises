f604220bbd78e589778703f3c8970d3458732ab5
sci-punk <ishen.sarabia@gmail.com>
2024-09-24 00:54:51 -0600

    tempConversion solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let celsius = (temperature - 32) * 5 / 9;
+  // round to 1 decimal
+  celsius = Math.round(celsius * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let fahrenheit = temperature * 9 / 5 + 32;
+  // round to 1 decimal
+  fahrenheit = Math.round(fahrenheit * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
