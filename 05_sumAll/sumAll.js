87646afc073f6653640f56b2c059c9132ccbf961
sci-punk <ishen.sarabia@gmail.com>
2024-09-24 00:39:50 -0600

    sumAll solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(...numbersToSum) {
+    let sum = 0;
+    // only allow positive and integer numbers
+    if (numbersToSum.some(num => num < 0 || !Number.isInteger(num))) {
+        return 'ERROR';
+    }
+    for (let i = Math.min(...numbersToSum); i <= Math.max(...numbersToSum); i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
