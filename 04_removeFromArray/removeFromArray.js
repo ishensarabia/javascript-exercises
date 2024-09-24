b94f16852ec636f5123f3a59858ac8d8713151d9
sci-punk <ishen.sarabia@gmail.com>
2024-09-24 00:32:24 -0600

    removeFromArray solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...valuesToRemove) {
+    return array.filter(value => !valuesToRemove.includes(value));
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
