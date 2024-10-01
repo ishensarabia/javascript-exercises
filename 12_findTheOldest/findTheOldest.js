416d2d6e48a12f62b139085ec6f8c99479962fe1
sci-punk <ishen.sarabia@gmail.com>
2024-09-30 23:16:40 -0600

    findTheOldest solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
 
+const findTheOldest = function(array) {
+                        //Accumulator //CurrentValue
+    return array.reduce((oldestPerson, currentPerson) => {
+        console.log(oldestPerson, currentPerson)
+        const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        const currentAge = getAge(
+          currentPerson.yearOfBirth,
+          currentPerson.yearOfDeath
+        );
+        return currentAge > oldestAge ? currentPerson : oldestPerson;
+      });
 };
 
 // Do not edit below this line
